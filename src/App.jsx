a0df48376af7ac883b2d58e00ab63db2b358dcec
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-08-27 15:15:56 +0600

    shopas ecommerce haf

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import Product from './components/product/Product'
+
+const App = () => {
+  return (
+    <div className=''>
+      <Navbar/>
+      <Hero/>
+      <Product/>
+    </div>
+  )
+}
+
+export default App  
---
+++ src/components/product/Product.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div>
+      <div>
+        {/* Header section */}
+
+        <div>
+            <p>Top selling product for you</p>
+            <h1>Product</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas mollitia magnam asperiores.</p>
+        </div>
+        {/* body section */}
+      </div>
+    </div>
+  )
+}
+
+export default Product
