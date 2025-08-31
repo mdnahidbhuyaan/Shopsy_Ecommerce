1000b6cc53723cd34fcefa13053fdb9bafe09b18
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-08-31 19:47:49 +0600

    top projuct  ane banner section complate

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,29 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Product from './components/product/Product'
+import AOS from 'aos'
+import TopProduct from './components/TopProduct/TopProduct'
+import Banner from './components/Banner/Banner'
 
 const App = () => {
+    React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
+  
   return (
     <div className=''>
       <Navbar/>
       <Hero/>
       <Product/>
+      <TopProduct/>
+      <Banner/>
     </div>
   )
 }
