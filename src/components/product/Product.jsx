881d8365e07e37ea1133139f763f66933e41b44f
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-08-31 08:47:41 +0600

    product

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -6,10 +6,10 @@ const Product = () => {
       <div>
         {/* Header section */}
 
-        <div>
+        <div className='text-center mb-10 max-w-[600px] mx-auto'>
             <p>Top selling product for you</p>
             <h1>Product</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas mollitia magnam asperiores.</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas mollitia magnam.</p>
         </div>
         {/* body section */}
       </div>
